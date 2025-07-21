d53624ba56cd7285686bf0a148fd9e0af68d5090
Brecht Vandersmissen <brecht@bootstrapped.ventures>
2025-07-21 11:33:36 +0200

    Proxy for Instacart API

---
+++ api/instacart.js
@@ -0,0 +1,59 @@
+export default async function handler(req, res) {
+  // Basic authentication.
+  const ALLOWED_SECRET = process.env.PROXY_SECRET;
+  const incomingSecret = req.headers['x-proxy-secret'];
+
+  if (incomingSecret !== ALLOWED_SECRET) {
+    return res.status(403).json({ error: 'Forbidden' });
+  }
+  
+  const INSTACART_API_KEY = process.env.INSTACART_API_KEY;
+
+  const instacartType = req.headers['x-instacart-type'] || 'recipe';
+
+  let endpoint = 'https://connect.instacart.com';
+  endpoint += '/idp/v1/';
+
+  if ( 'shopping_list' === instacartType ) {
+    endpoint += 'products/products_link';
+  } else {
+    // Default to recipe endpoint.
+    endpoint += 'products/recipe';
+  }
+
+  // Analytics
+  const siteUrl = req.headers['x-site-url'] || 'unknown';
+  const pluginVersion = req.headers['x-plugin-version'] || 'unknown';
+
+  // Log usage to Better Stack.
+  const BETTERSTACK_URL = process.env.BETTERSTACK_URL;
+  const BETTERSTACK_KEY = process.env.BETTERSTACK_KEY;
+
+  fetch( BETTERSTACK_URL, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${BETTERSTACK_KEY}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      message: `Instacart API`,
+      feature: `instacart-${instacartType}`,
+      site: siteUrl,
+      version: pluginVersion,
+    }),
+  }).catch(console.error); // Don’t block proxy if logging fails
+
+  // Proxy the API call.
+  const response = await fetch( endpoint, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${INSTACART_API_KEY}`,
+      'Content-Type': 'application/json',
+      'accept': 'application/json',
+    },
+    body: JSON.stringify(req.body),
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
